03c262eb4fc9ebd5bfd986b31e6e92f03664ce92
Azham1999 <nmazham320@gmail.com>
2025-06-28 20:56:46 +0530

    Commit initial files

---
+++ index.js
@@ -0,0 +1,44 @@
+let Add = document.getElementById("score")
+let startAgain = document.getElementById("restart")
+
+let count = 0
+
+function homeaddone() {
+    count = count + 1
+    Add.innerText = count
+    console.log(count)
+}
+
+function homeaddtwo() {
+    count = count + 2
+    Add.innerText = count
+    console.log(count)
+}
+
+function homeaddthree() {
+    count = count + 3
+    Add.innerText = count
+    console.log(count)
+}
+
+let Add2 = document.getElementById("score2")
+
+let count2 = 0
+
+function awayaddone() {
+    count2 = count2 + 1
+    Add2.innerText = count2
+    console.log(count2)
+}
+
+function awayaddtwo() {
+    count2 = count2 + 2
+    Add2.innerText = count2
+    console.log(count2)
+}
+
+function awayaddthree() {
+    count2 = count2 + 3
+    Add2.innerText = count2
+    console.log(count2)
+}
